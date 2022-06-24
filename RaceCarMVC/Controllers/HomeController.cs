de4fe0af7db8042404f182ba528634176c08f313
Channing Robertson <channing.robertson@gmail.com>
2022-06-24 07:54:05 -0500

    Add project files.

---
+++ RaceCarMVC/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaceCarMVC.Models;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace RaceCarMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reverse()
+        {
+            Palindrome model = new Palindrome();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Reverse(Palindrome palindrome)
+        {
+            string inputWord = palindrome.InputWord;
+            string revWord = "";
+
+            for (int i = inputWord.Length - 1; i >= 0; i--)
+            {
+                revWord += inputWord[i];
+            }
+
+            palindrome.RevWord = revWord;
+
+            revWord = Regex.Replace(revWord.ToLower(), "[^a-zA-Z0-9]+", "");
+            inputWord = Regex.Replace(inputWord.ToLower(), "[^a-zA-Z0-9]+", "");
+
+            if (revWord == inputWord)
+            {
+                palindrome.IsPalindrome = true;
+                palindrome.Message = $"Success {palindrome.InputWord} is a palindrome";
+            }
+            else
+            {
+                palindrome.IsPalindrome = false;
+                palindrome.Message = $"Sorry, {palindrome.InputWord} is not a palindrome";
+            }
+
+            return View(palindrome);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RaceCarMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RaceCarMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
